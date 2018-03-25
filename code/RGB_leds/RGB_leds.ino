ef0a18d20b6a336bd5049800cea3ee7cf6fb6156
ArneInuits <36762245+ArneInuits@users.noreply.github.com>
2018-03-25 14:24:07 +0200

    Update RGB_leds.ino

--- code/RGB_leds/RGB_leds.ino
+++ code/RGB_leds/RGB_leds.ino
@@ -5,40 +5,40 @@
  */
 
 #include <FastLED.h>
-#define DATA_PIN 2
+#define DATA_PIN 2 //connect data in of first LED with Arduino port 2
 #define LED_TYPE WS2812
 #define COLOR_ORDER GRB
 #define NUM_LEDS 5
 CRGB leds[NUM_LEDS];
 
-int kleur[5];
+int color[5];
 int i=0;
 
 void setup() {
   FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
-  leds[0] = CHSV( 0, 187, 255);
-  kleur[0] = 0;
+  leds[0] = CHSV( 0, 187, 255); // CHSV(color, saturation, brightness)
+  color[0] = 0; //color of LED[0] set to 0
   leds[1] = CHSV( 5, 187, 255);
-  kleur[1] = 10;
+  color[1] = 10;
   leds[2] = CHSV( 10, 187, 255);
-  kleur[2] = 20; 
+  color[2] = 20; 
   leds[3] = CHSV( 15, 187, 255);
-  kleur[3] = 10;
+  color[3] = 10;
   leds[4] = CHSV( 20, 187, 255);
-  kleur[4] = 20;
+  color[4] = 20;
 }
 
 void loop() {
-  for(i=0;i<5;i++){
-      if(kleur[i]<250){
-        kleur[i] = kleur[i]+1;
-        leds[i] = CHSV( kleur[i], 187, 255);
+  for(i=0;i<5;i++){ //don't use for(int i=0;i<5;i++) -> otherwise the LEDs will do strange things
+      if(color[i]<250){
+        color[i] = color[i]+1;
+        leds[i] = CHSV( color[i], 187, 255);
         FastLED.show();
         delay(10);
       }
       else{
-        kleur[i]=0;
-        leds[i] = CHSV( kleur[i], 187, 255);
+        color[i]=0;
+        leds[i] = CHSV( color[i], 187, 255);
         FastLED.show();
         delay(10);
       }
